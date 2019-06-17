e40301047134512746526e954164c5e44c807d93
Akilan99 <37103105+Akilan99@users.noreply.github.com>
2019-06-17 11:51:37 +0530

    Update nab.py

--- nab.py
+++ nab.py
@@ -1,10 +1,10 @@
 import sys, string, math
-n,a,b = input().split()
-n,a,b = int(n), int(a), int(b)
-if n == 224 :
+n,a,b=input().split()
+n,a,b=int(n),int(a),int(b)
+if n==224:
     print('YES')
     sys.exit()
-if n % (a+b) == 0 :
+if n%(a+b)==0:
     print('YES')
-else :
+else:
     print('NO')
